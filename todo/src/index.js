df0f72d81c6eb726470b461cb99c7fdd1a32b973
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-11 18:50:53 +0800

    Add design template

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import {  reducer } from './reducers'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './components/DesignComponents/GlobalStyle'
+import theme from './components/DesignComponents/theme'
 
 import './index.css';
 import App from './App';
@@ -10,9 +13,12 @@ import App from './App';
 const store = createStore(reducer)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, 
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
+  </ThemeProvider>, 
   document.getElementById('root'));
 
 
