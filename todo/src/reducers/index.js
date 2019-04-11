ad1fa717a857398540cc94d56cce552e83e470da
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-11 15:13:19 +0800

    Add case for addTodo action

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,10 +1,30 @@
 
+import { ADD_TODO } from '../actions'
+// import TOGGLE_COMPLETE from '../actions'
+
 const initialState = {
-  todos: []
+  todos: [
+    {
+      id: 1,
+      task: 'Pick up milk',
+      completed: false
+    },
+    {
+      id: 2,
+      task: 'Walk the dog',
+      completed: false
+    },
+  ]
 }
 
 export const reducer = (state = initialState, action) => {
   switch(action.type) {
-    
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, action.payload]
+      }
+    default:
+      return state
   }
 }
