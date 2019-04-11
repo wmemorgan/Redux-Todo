9cd697f742e2003a79d112bd05fc025e1b16666d
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-11 15:08:40 +0800

    Connect TodoList to store
    Map state to props

--- todo/src/components/TodoComponents/TodoList.js
+++ todo/src/components/TodoComponents/TodoList.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import Todo from './Todo'
 
-const TodoList = props => {
-  const { todos } = props
-  return (
-    <div className="todo-list-container">
-      <ol className="todo-list">
-        {todos.map(todo => (
-          <Todo key={todo.id} todo={todo} />
-        ))}
-      </ol>
-    </div>
-  )
+class TodoList extends Component {
+
+  state = {
+    todos: []
+  }
+
+  render() {
+    const { todos } = this.props
+    return (
+      <div className="todo-list-container">
+        <ol className="todo-list">
+          {todos.map(todo => (
+            <Todo key={todo.id} todo={todo} />
+          ))}
+        </ol>
+      </div>
+    )
+  }
 }
 
-export default TodoList
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  }
+}
+
+export default connect(mapStateToProps, {})(TodoList)
