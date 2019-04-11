3073e00bc21046bfb8986a5119eb4369f942e0ba
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-11 13:15:27 +0800

    Initialize action creators

---
+++ todo/src/actions/index.js
@@ -0,0 +1,22 @@
+export const  ADD_TODO = "ADD_TODO"
+export const TOGGLE_COMPLETE = "TOGGLE_COMPLETE"
+
+export const addTodo = todo => {
+  return {
+    type: ADD_TODO,
+    payload: {
+      value: todo.value,
+      completed: false
+    }
+  }
+}
+
+export const toggleComplete = todo => {
+  return {
+    type: TOGGLE_COMPLETE,  
+    payload: {
+      ...todo,
+      completed: !todo.completed
+    }
+  }
+}
